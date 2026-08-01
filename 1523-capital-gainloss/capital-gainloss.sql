043a32c6a8083e1625680ac6ddb8a573ed48aacb
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-08-01 18:58:48 +0530

    Time: 774 ms (7.48%) | Memory: 0B (100.00%) - LeetSync

--- 1523-capital-gainloss/capital-gainloss.sql
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -1,10 +1,12 @@
 # Write your MySQL query statement below
 select stock_name,
-    sum(
-        case 
-            when operation="buy" then -price
-            else price
-        end
-    ) as capital_gain_loss
+    -- sum(
+    --     case 
+    --         when operation="buy" then -price
+    --         else price
+    --     end
+    -- ) as capital_gain_loss
+
+    sum(if(operation="buy",-price,price)) as capital_gain_loss
 
     from Stocks group by stock_name;
