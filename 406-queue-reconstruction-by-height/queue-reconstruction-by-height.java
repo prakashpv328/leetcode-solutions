00bbb34874918355cf8d47d49559aa219d5f244c
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-08-11 08:46:47 +0530

    Time: 13 ms (10.39%) | Memory: 47.3 MB (47.42%) - LeetSync

---
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        ArrayList<int[]> list = new ArrayList<>();
+        int n=people.length;
+        Arrays.sort(people,(a,b)->{
+            if(a[0]!=b[0])
+                return b[0]-a[0];
+            return a[1]-b[1];
+        });
+        System.out.print(Arrays.deepToString(people));
+           int[][] res = new int[n][2];
+         int i=0;
+        for(int[] a:people){
+            list.add(a[1],a);
+          
+        }
+        
+        return list.toArray(new int[0][]);
+        
+    }
+}
