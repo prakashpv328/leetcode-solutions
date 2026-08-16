fae26ba1472f9c13f711f261f2df702c91d2f05b
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-08-16 15:09:43 +0530

    Time: 72 ms (92.63%) | Memory: 155.9 MB (53.68%) - LeetSync

---
+++ 3500-minimum-cost-for-cutting-cake-ii/minimum-cost-for-cutting-cake-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public long minimumCost(int m, int n, int[] hCut, int[] vCut) {
+        int hl=hCut.length;
+        int vl=vCut.length;
+        Arrays.sort(vCut);
+        Arrays.sort(hCut);
+        int i=vl-1,j=hl-1;
+        int nv=1,nh=1;
+        long ans=0;
+        while(i>=0 && j>=0){
+            if(vCut[i]>hCut[j]){
+                ans+=vCut[i--]*nh;
+                nv++;
+            }
+            else{
+                ans+=hCut[j--]*nv;
+                nh++;
+            }
+        }
+        while(i>=0){
+            ans+=vCut[i--]*nh;
+            nv++;
+        }
+        while(j>=0){
+            ans+=hCut[j--]*nv;
+            nh++;
+        }
+        return ans;
+    }
+}
