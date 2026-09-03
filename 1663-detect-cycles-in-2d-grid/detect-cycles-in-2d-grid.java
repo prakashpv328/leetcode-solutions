6f2f79070a77b2173cee3dd0cbde61550c51c478
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-09-03 09:33:27 +0530

    Time: 27 ms (8.08%) | Memory: 160.4 MB (5.57%) - LeetSync

---
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean containsCycle(char[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int vis[][]=new int[m][n];
+        int path[][]=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(vis[i][j]==0 && dfs(i,j,-1,-1,grid,vis,path,grid[i][j])){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean dfs(int r ,int c,int i,int j,char[][] grid,int [][]vis,int[][] path,char ch){
+        vis[r][c]=1;
+        path[r][c]=1;
+        int m=grid.length;
+        int n=grid[0].length;
+        int[][] dir={{-1,0},{0,1},{1,0},{0,-1}};
+        for(int x=0;x<4;x++){
+            int nr=r+dir[x][0];
+            int nc=c+dir[x][1];
+            if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==ch){
+                if(nr==i && nc==j) continue;
+                if(vis[nr][nc]==0 && dfs(nr,nc,r,c,grid,vis,path,grid[nr][nc])){
+                    return true;
+                }
+                else if(path[nr][nc]!=0){
+                    return true;
+                }
+            }
+        }
+        path[r][c]=0;
+        return false;
+    }
+}
