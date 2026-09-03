7729bf92addf29a478178adde80d0c697b454ab1
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-09-03 09:35:35 +0530

    Time: 26 ms (11.05%) | Memory: 158.1 MB (5.57%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
@@ -6,16 +6,15 @@ class Solution {
         int path[][]=new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(vis[i][j]==0 && dfs(i,j,-1,-1,grid,vis,path,grid[i][j])){
+                if(vis[i][j]==0 && dfs(i,j,-1,-1,grid,vis,grid[i][j])){
                     return true;
                 }
             }
         }
         return false;
     }
-    private boolean dfs(int r ,int c,int i,int j,char[][] grid,int [][]vis,int[][] path,char ch){
+    private boolean dfs(int r ,int c,int i,int j,char[][] grid,int [][]vis,char ch){
         vis[r][c]=1;
-        path[r][c]=1;
         int m=grid.length;
         int n=grid[0].length;
         int[][] dir={{-1,0},{0,1},{1,0},{0,-1}};
@@ -24,15 +23,15 @@ class Solution {
             int nc=c+dir[x][1];
             if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==ch){
                 if(nr==i && nc==j) continue;
-                if(vis[nr][nc]==0 && dfs(nr,nc,r,c,grid,vis,path,grid[nr][nc])){
+                if(vis[nr][nc]==0){
+                    if(dfs(nr,nc,r,c,grid,vis,grid[nr][nc]))
                     return true;
                 }
-                else if(path[nr][nc]!=0){
+                else{
                     return true;
                 }
             }
         }
-        path[r][c]=0;
         return false;
     }
 }
