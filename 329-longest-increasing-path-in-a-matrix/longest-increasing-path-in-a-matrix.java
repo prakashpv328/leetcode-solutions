147f6c8f75a70c14440419c45a7e614342662a5f
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-09-05 21:04:03 +0530

    Time: 11 ms (21.22%) | Memory: 47.3 MB (29.53%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int longestIncreasingPath(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int num=0;
+        int ans=0;
+
+        int dp[][]=new int[m][n]; 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                num=dfs(i,j,matrix,dp);
+                ans=Math.max(num,ans);
+            }
+        }
+        return ans;
+    }
+
+    public int dfs(int r,int c,int [][]matrix,int[][] dp){
+        if(dp[r][c]!=0){
+            return dp[r][c];
+        }
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int [][]dir={{-1,0},{0,1},{1,0},{0,-1}};
+        int num=0;
+        for(int x=0;x<4;x++){
+            int nr=r+dir[x][0];
+            int nc=c+dir[x][1];
+            if(nr>=0 && nr<m && nc>=0 && nc<n && matrix[nr][nc]>matrix[r][c]){
+                num=Math.max(num,dfs(nr,nc,matrix,dp));
+            }
+        }
+        dp[r][c]=1+num;
+        return dp[r][c];
+    }
+}
