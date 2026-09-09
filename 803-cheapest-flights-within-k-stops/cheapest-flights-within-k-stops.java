0ff69b57f4f0b87cfc60c7aeeeb571362ad80e9f
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-09-09 11:53:40 +0530

    Time: 10 ms (23.13%) | Memory: 46.5 MB (69.61%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,10 +1,20 @@
 class Solution {
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int l=flights.length;
-        List<List<int[]>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++){
+    public int findCheapestPrice(int n, int[][] flights,
+                                 int src, int dst, int k) {
+
+        List<List<int[]>> adj = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
             adj.add(new ArrayList<>());
         }
+
+        for(int[] f : flights){
+            adj.get(f[0]).add(new int[]{f[1], f[2]});
+        }
+
+        // node, price, flights
+        PriorityQueue<int[]> pq =
+            new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
 
         int[][] dist = new int[n][k + 2];
 
@@ -12,33 +22,46 @@ class Solution {
             Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
 
-        for(int []f:flights){
-            adj.get(f[0]).add(new int[]{f[1],f[2]});
-        }
+        dist[src][0] = 0;
 
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
-        pq.offer(new int[]{src,0,0});
+        pq.offer(new int[]{src, 0, 0});
+
         while(!pq.isEmpty()){
-            int arr[]=pq.poll();
-            int node=arr[0];
-            int amount=arr[1];
-            int turn=arr[2];
-            if(node==dst){
-                return amount;
+
+            int[] curr = pq.poll();
+
+            int node = curr[0];
+            int price = curr[1];
+            int flightsTaken = curr[2];
+
+            if(node == dst){
+                return price;
             }
-            if(turn==k+1){
+
+            if(flightsTaken == k + 1){
                 continue;
             }
-            for(int []next:adj.get(node)){
-                int nextNode=next[0];
-                int nextPrice=next[1]+amount;
-                int nextTurn=turn+1;
-                if(nextPrice<dist[nextNode][nextTurn]){
-                    dist[nextNode][nextTurn]=nextPrice;
-                    pq.offer(new int[]{nextNode,nextPrice,nextTurn});
+
+            for(int[] next : adj.get(node)){
+
+                int nextNode = next[0];
+                int nextPrice = price + next[1];
+                int nextFlights = flightsTaken + 1;
+
+                // Only process if this is better
+                if(nextPrice < dist[nextNode][nextFlights]){
+
+                    dist[nextNode][nextFlights] = nextPrice;
+
+                    pq.offer(new int[]{
+                        nextNode,
+                        nextPrice,
+                        nextFlights
+                    });
                 }
             }
         }
+
         return -1;
     }
 }
