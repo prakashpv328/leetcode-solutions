db77599048518477c0d43b8b0f00896774d39395
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-09-15 11:25:28 +0530

    Time: 8 ms (35.02%) | Memory: 47.8 MB (96.72%) - LeetSync

---
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int nearestExit(char[][] maze, int[] entrance) {
+        int m=maze.length;
+        int n=maze[0].length;
+        Queue<int[]> queue=new LinkedList<>();
+        queue.offer(new int[]{entrance[0],entrance[1],0});
+        maze[entrance[0]][entrance[1]]='+';
+        int [][]dir={{-1,0},{0,1},{1,0},{0,-1}};
+        while(!queue.isEmpty()){
+            int []arr=queue.poll();
+            int i=arr[0];
+            int j=arr[1];
+            int step=arr[2];
+            for(int x=0;x<4;x++){
+                int nr=i+dir[x][0];
+                int nc=j+dir[x][1];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && maze[nr][nc]=='.'){
+                    maze[nr][nc]='+';
+                    if(nr==0 || nr==m-1 || nc==0 || nc==n-1 ) return step+1;
+                    queue.offer(new int[]{nr,nc,step+1});
+                }
+            }
+        }
+        return -1;
+    }
+}
