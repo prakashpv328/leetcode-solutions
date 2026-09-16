c842997e1e2f62962eedb2c642259d0827d5e81f
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-09-16 20:43:05 +0530

    Time: 1054 ms (5.03%) | Memory: 58.7 MB (5.19%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    int ans=0;
+    int [][]dp;
+    int l;
+    public int change(int amount, int[] coins) {
+        l=coins.length;
+        dp=new int[amount+1][l];
+        for(int []row:dp)
+        Arrays.fill(row,-1);
+
+        return solve(amount,coins,0,0);
+    }
+    private int solve(int amount,int[] coins,int i,int sum){
+        if(sum>=amount || i==l){
+            if(sum==amount){
+                return 1;
+            }
+            return 0;
+        }
+        if(dp[sum][i]!=-1) return dp[sum][i];
+        int n=0;
+        for(int x=i;x<l;x++){
+            n+=solve(amount,coins,x,sum+coins[x]);
+        }
+        return dp[sum][i]=n;
+    }
+}
