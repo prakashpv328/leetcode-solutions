288881249c1b8c163803376cdf458650b36ff56a
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-09-16 20:45:38 +0530

    Time: 1080 ms (5.03%) | Memory: 54.2 MB (61.60%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,49 +1,26 @@
 class Solution {
-    int[][] dp;
-
+    int [][]dp;
+    int l;
     public int change(int amount, int[] coins) {
+        l=coins.length;
+        dp=new int[amount+1][l];
+        for(int []row:dp)
+        Arrays.fill(row,-1);
 
-        int n = coins.length;
-
-        dp = new int[amount + 1][n];
-
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
-
-        return solve(amount, coins, 0, 0);
+        return solve(amount,coins,0,0);
     }
-
-    private int solve(int amount, int[] coins, int i, int sum) {
-
-        if (sum == amount) {
-            return 1;
-        }
-
-        if (sum > amount || i == coins.length) {
+    private int solve(int amount,int[] coins,int i,int sum){
+        if(sum>=amount || i==l){
+            if(sum==amount){
+                return 1;
+            }
             return 0;
         }
-
-        if (dp[sum][i] != -1) {
-            return dp[sum][i];
+        if(dp[sum][i]!=-1) return dp[sum][i];
+        int n=0;
+        for(int x=i;x<l;x++){
+            n+=solve(amount,coins,x,sum+coins[x]);
         }
-
-        // Take current coin
-        int take = solve(
-            amount,
-            coins,
-            i,
-            sum + coins[i]
-        );
-
-        // Skip current coin
-        int skip = solve(
-            amount,
-            coins,
-            i + 1,
-            sum
-        );
-
-        return dp[sum][i] = take + skip;
+        return dp[sum][i]=n;
     }
 }
