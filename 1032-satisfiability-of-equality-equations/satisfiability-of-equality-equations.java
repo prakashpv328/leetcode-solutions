164651992265c57ac2684e828f99f2f0a690e334
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-09-17 12:05:51 +0530

    Time: 1 ms (95.81%) | Memory: 44 MB (26.93%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
@@ -6,7 +6,9 @@ class Solution {
             if(s.charAt(1)=='='){
                 int u=s.charAt(0)-'a';
                 int v=s.charAt(3)-'a';
-                ds.union(u,v);
+                int pu=ds.find(u);
+                int pv=ds.find(v);
+                if(pu!=pv) ds.union(u,v);
             }
         }
 
