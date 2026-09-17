530581d94b6bd705a216c1ee792f2a2eeaf80622
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-09-17 10:59:15 +0530

    Time: 4 ms (89.08%) | Memory: 66.2 MB (53.65%) - LeetSync

---
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        DisjointSet ds=new DisjointSet(n+1);
+        for(int i=0;i<connections.length;i++){
+            ds.unionByRank(connections[i][0],connections[i][1]);
+        }
+        int com=0;
+        for(int i=0;i<n;i++){
+            if(ds.parent[i]==i){
+                com++;
+            }
+        }
+        if(connections.length<(n-1)) return -1;
+        return com-1;
+    }
+}
+
+class DisjointSet{
+    int []parent;
+    int []rank;
+    DisjointSet(int n){
+        parent=new int[n];
+        rank=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            rank[i]=0;
+        }
+    }
+
+    public int findUPar(int node){
+        if(parent[node]==node){
+            return node;
+        }
+        return parent[node]=findUPar(parent[node]);
+    }
+
+    public void unionByRank(int u,int v){
+        int rootU=findUPar(u);
+        int rootV=findUPar(v);
+        if(rank[rootU]<rank[rootV]){
+            parent[rootU]=rootV;
+        }
+        else if(rank[rootU]>rank[rootV]){
+            parent[rootV]=rootU;
+        }
+        else{
+            parent[rootV]=rootU;
+            rank[rootU]++;
+        }
+    }
+}
