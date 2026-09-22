92429a44fee6ead329d3db4a0fd14bbd452221a9
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-09-22 19:22:11 +0530

    Time: 0 ms (100.00%) | Memory: 50.6 MB (11.54%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n=citations.length;
+        int l=0,r=n-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(citations[m]>=n-m){
+                r=m-1;
+            }
+            else{
+                l=m+1;
+            }
+        }
+        return n-l;
+    }
+}
