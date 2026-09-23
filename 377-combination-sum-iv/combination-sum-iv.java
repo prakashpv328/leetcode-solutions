e317fb3b29039f5a2d4e87e9a58106bd49cc6e2d
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-09-23 16:05:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (27.77%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.java
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -1,10 +1,6 @@
 class Solution {
-    int ans=0;
     int[] dp;
     public int combinationSum4(int[] nums, int target) {
-        int count=0;
-        int sum=0;
-        int tot=0;
         dp=new int[target+1];
         Arrays.fill(dp,-1);
         return funct(0,target,nums);
