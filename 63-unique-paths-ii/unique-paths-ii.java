4c7306d01ebf4172a22d02feb4b084e13b166704
prakashpv328 <prakashpv.cs24@bitsathy.ac.in>
2026-09-25 13:30:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (46.07%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -4,13 +4,14 @@ class Solution {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
         dp=new int[m][n];
+        for(int i=0;i<m;i++) Arrays.fill(dp[i],-1);
         return solve(m-1,n-1,obstacleGrid);
     }
 
     private int solve(int i,int j,int [][]grid){
         if(i<0 || j<0 || grid[i][j]==1) return 0;
+        if(dp[i][j]!=-1) return dp[i][j]; 
         if(i==0 && j==0) return 1;
-        if(dp[i][j]!=0) return dp[i][j]; 
         int left=solve(i,j-1,grid);
         int up=solve(i-1,j,grid);
         return dp[i][j]=left+up;
